032867e2419826c33afdf2ea01aff6f0a3e8108f
Azharul Islam <lonewolfazharul@gmail.com>
2023-04-26 15:10:41 +0600

    added qr codes

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,12 +1,19 @@
+import Image from 'next/image';
 import React from 'react';
+import barcode from "../../../public/images/WhatsApp Image 2023-04-12 at 3.04.04 PM (1).jpeg"
+import barcode1 from "../../../public/images/WhatsApp Image 2023-04-12 at 3.04.04 PM.jpeg"
 
 const Footer = () => {
     return (
-        <footer className="p-8 mt-14 footer footer-center mb-0 bg-red-700 text-white dark:bg-slate-800 dark:text-white">
-        <div>
+        <footer className="flex justify-around items-center p-8 mt-14 footer mb-0 bg-red-700 text-white dark:bg-slate-800 dark:text-white">
+            <div>
+                <Image src={barcode} height="100" width="100" />
+            </div>
             <p>Copyright © 2023 - All right reserved by Red Apple</p>
-        </div>
-    </footer>
+            <div>
+                <Image src={barcode1} height="100" width="100" />
+            </div>
+        </footer>
     );
 };
 
