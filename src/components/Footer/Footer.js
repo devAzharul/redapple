12cbe74dad02548c352ef8b0be9bf97a77d4c19b
Azharul Islam <www.lonewolfazharul@gmail.com>
2023-05-08 17:42:03 +0600

    footer merge

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,13 +1,21 @@
 import Link from 'next/link';
 import React from 'react';
+import barcode from "../../../public/images/WhatsApp Image 2023-04-12 at 3.04.04 PM (1).jpeg"
+import barcode1 from "../../../public/images/WhatsApp Image 2023-04-12 at 3.04.04 PM.jpeg"
+import Image from 'next/image';
 
 const Footer = () => {
     return (
-        <footer className="p-8 mt-14 footer footer-center mb-0 bg-red-700 text-white dark:bg-slate-800 dark:text-white">
+        <footer className="flex justify-around items-center p-8 mt-14 footer mb-0 bg-red-700 text-white dark:bg-slate-800 dark:text-white">
             <div>
-                <p>Copyright © 2023 - All right reserved by <Link href=''>DevAzharul</Link></p>
+                <Image src={barcode} height="100" width="100" />
+            </div>
+            <Link href='https://www.facebook.com/store.techhunt' target="_blank"><p>Copyright © 2023 - All right reserved by Tech-Hunt</p></Link>
+            <div>
+                <Image src={barcode1} height="100" width="100" />
             </div>
         </footer>
+
     );
 };
 
