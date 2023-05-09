6f0f07b610e49d63b1c7c47dbb7368d4bdd99233
Azharul Islam <www.lonewolfazharul@gmail.com>
2023-05-09 16:43:33 +0600

    about section completed

--- pages/AboutScreen.js
+++ pages/AboutScreen.js
@@ -4,7 +4,82 @@ import Layout from './Layout/Layout';
 const AboutScreen = () => {
     return (
         <Layout title="About">
-            <div>This is about</div>
+            <section className="my-8">
+                <div className="container mx-auto flex flex-col items-center pb-6 mb-4 md:p-10 md:px-12">
+                    <h1 className="text-2xl font-semibold leading-none text-center">Before going any further, be prepared to keep aside everything you know about the way traditional agencies work with their clients. Redapple is a group of unique meditating head who will re-define your branding in order to take it in a new height. Group members come from a diverse range of background, both marketing and non-marketing, but each with independent set of expertise that are invaluable. The great advantage of such a formation is that clients are no longer limited to just the people within the agency redapple lets you to tap into the best strategic minds as well as a surplus of creative production resources.</h1>
+                </div>
+                <div className="container mx-auto grid grid-cols-1 gap-8 lg:gap-20 md:px-10 md:pb-10 lg:grid-cols-2">
+                    <div className="flex flex-col items-center mx-12 lg:mx-0">
+                        <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
+                        <p className="text-2xl font-semibold">Our Believe</p>
+                        <div className="relative text-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute top-0 left-0 w-8 h-8 dark:text-gray-700">
+                                <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                            </svg>
+                            <p className="px-6 py-1 text-lg italic">Talk to people on the roads via our LED Screens and signboards. Reach them on their profiles on social Media through our social Media Marketing services.
+
+                                Whether you are organizing a concert, exhibition or any event that incorporates any kind of printing, Audiovisual, Lighting, Decorating, Flower works, Installations, Logistics, LED, Projections, Photography, Artist Management, anything starting from the smallest (business cards) to the largest format setup (nationwide campaign), our high quality and on budget services is the best for you.</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
+                                <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0">
+                        <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
+                        <p className="text-2xl font-semibold">Our Strength</p>
+                        <div className="relative text-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute top-0 left-0 w-8 h-8 dark:text-gray-700">
+                                <path fill="currentColor" d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                <path fill="currentColor" d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                            </svg>
+                            <p className="px-6 py-1 text-lg italic">As an advertising agency, redapple provides a service. But so do mechanics, dry cleaners and massage parlors. The difference is that they give you a menu of services to choose from. We don't, because every client represents a new brand, a new set of circumstances and a new opportunity—no two strategies are ever the same.
+                                Instead, our services are only recommended once we've established a strategy for mutating your brand from traditional media to the digital realm. Whether that strategy includes a website overhaul supplemented by traditional advertising or a social media blitz coupled with web video is yet to be seen.</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
+                                <path fill="currentColor" d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                <path fill="currentColor" d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+                <div className="container mx-auto grid grid-cols-1 gap-8 lg:gap-20 md:px-10 md:pb-10 lg:grid-cols-2">
+                    <div className="flex flex-col items-center mx-12 lg:mx-0">
+                        <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
+                        <p className="text-2xl font-semibold">Our Philosophy</p>
+                        <div className="relative text-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute top-0 left-0 w-8 h-8 dark:text-gray-700">
+                                <path d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                <path d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                            </svg>
+                            <p className="px-6 py-1 text-lg italic">Ask someone what advertising is and you'll most likely hear about television commercials, newspaper ads and radio spots. But we know it's more than that. It's change. It's the fight between trying to ignore it and trying to fit it into your life. It's everywhere and it's what we do.
+
+                                While traditional advertising is important (those TV spots and radio commercials), we don't stop there. Studio CMP takes a look at where your customers are and designs marketing plans targeted towards those spaces. There's no sense in you being where you're customers aren't. Your opportunities exist in both what is well known and what is unknown. They exist in the smallest of details and the largest of concepts. And they're all interconnected. Just because you've never done that, doesn't mean you shouldn't.</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
+                                <path d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                <path d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                    <div className="flex flex-col items-center max-w-lg mx-12 lg:mx-0">
+                        <span className="w-12 h-1 my-2 rounded-lg dark:bg-violet-400"></span>
+                        <p className="text-2xl font-semibold">Mission & Vision</p>
+                        <div className="relative text-center">
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute top-0 left-0 w-8 h-8 dark:text-gray-700">
+                                <path fill="currentColor" d="M232,246.857V16H16V416H54.4ZM48,48H200V233.143L48,377.905Z"></path>
+                                <path fill="currentColor" d="M280,416h38.4L496,246.857V16H280ZM312,48H464V233.143L312,377.905Z"></path>
+                            </svg>
+                            <p className="px-6 py-1 text-lg italic"> Redapple mission is to serve our clients better in this respect we are engaged in involving all types of latest technology innovation. Redapple wants to provide a great service to build up an inspired and elegant relation with the superior client on the basis of deep understanding of behavior by following theory. Deliver result-oriented, creative, communicative advertising and branding strategies and solutions to add distinctive value to the brand. Transform strategy, creativity and market research analytics into brilliant ideas that sell our clients products. We seek to partner our clients.
+
+                                Become the most sought after advertising and branding company across the nation with high creativity, performance and customer responsiveness.</p>
+                            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="absolute bottom-0 right-0 w-8 h-8 dark:text-gray-700">
+                                <path fill="currentColor" d="M280,185.143V416H496V16H457.6ZM464,384H312V198.857L464,54.1Z"></path>
+                                <path fill="currentColor" d="M232,16H193.6L16,185.143V416H232ZM200,384H48V198.857L200,54.1Z"></path>
+                            </svg>
+                        </div>
+                    </div>
+                </div>
+            </section>
         </Layout>
     );
 };
